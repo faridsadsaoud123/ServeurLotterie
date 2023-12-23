0676d2589beebee2f96e911aa79d6e4682199e64
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-12-23 18:52:02 +0100

    generateNumeroSerie method

--- src/Billet.java
+++ src/Billet.java
@@ -1,9 +1,24 @@
 import java.io.Serializable;
+import java.security.SecureRandom;
 import java.util.List;
 
 public class Billet implements Serializable {
     private String numeroSerie;
     private List<Integer> numerosChoisis;
+    public Billet(){
+        this.numeroSerie = generateNumeroSerie();
+    }
+    public static String generateNumeroSerie() {
+        String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int SERIE_LENGTH = 20;
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(SERIE_LENGTH);
 
+        for (int i = 0; i < SERIE_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
 
+        return sb.toString();
+    }
 }
